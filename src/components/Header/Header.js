98d7707dfe616b36d817b9eb196c83742da4a73c
stcastell <santicastell98@hotmail.com>
2023-05-30 20:12:17 -0400

    Components folder created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => { 
+    return (
+        <header>
+            <h1>HELLOOO</h1>
+        </header>
+    )
+}
+
+export default Header
