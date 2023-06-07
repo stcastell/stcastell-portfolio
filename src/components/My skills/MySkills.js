97ac73e089c0bf92a231ce681dc22549e1c7ebca
stcastell <santicastell98@hotmail.com>
2023-06-06 23:29:48 -0400

    Minor changes

--- src/components/My skills/MySkills.js
+++ src/components/My skills/MySkills.js
@@ -5,8 +5,29 @@ const MySkills = () => {
 
     return (
         <div className="my-skills-container">
+            <h2>My skills</h2>
             <div className="skills-icons">
-                A
+
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" />
+                
+                <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" />
+                
+                <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" />
+                
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" />
+                
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" />
+                
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" />
+                
+                <img src="https://www.vectorlogo.zone/util/preview.html?image=/logos/reactjs/reactjs-icon.svg" alt="React.js" />
+                
+                <img src="https://github.com/devicons/devicon/blob/master/icons/php/php-original.svg" alt="PHP" />
+                
+                <img src="https://github.com/devicons/devicon/blob/master/icons/mysql/mysql-original.svg" alt="MySQL" />
+                
+                <img src="https://github.com/devicons/devicon/blob/master/icons/python/python-original.svg" alt="Python" /> 
+                
             </div>
         </div>
     );
