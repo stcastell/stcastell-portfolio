6a7eb2a9a16b5cf8a5468c26479f7ed45ed5b76b
stcastell <santicastell98@hotmail.com>
2023-06-11 21:48:59 -0400

    Updated

--- src/components/My projects/MyProjects.js
+++ src/components/My projects/MyProjects.js
@@ -3,12 +3,22 @@ import styles from './MyProjects.module.css'
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css/skyblue';
 
-const MyProjects = () => { 
+const MyProjects = () => {  
 
     return (
         <Splide aria-label="My Favorite Images" options={{
+            type: 'slide',
             rewind: true,
+            perPage: 1,
+            perMove: 1,
             gap: '1rem',
+            pagination: true,
+            arrows: true,
+            speed: 800,
+            pauseOnHover: true,
+            autoplay: true,
+            interval: 500,
+            easing: 'cubic-bezier(.42,.65,.27,.99)',
         }} className={styles['projects-container']}>
             <SplideSlide className={styles['img-container']}>
                 <img src="https://img001.prntscr.com/file/img001/naBL2BiiQbyNExMc6Ri4DA.png" alt="Lightshot screenshot" id="screenshot-image" image-id="2aqdyo1"/>
